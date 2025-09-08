ab42118d8230108c69c0297f4e46ac243a901972
zihanpeter <luzihan1104@163.com>
2025-09-08 11:44:19 +0800

    constants & tuning templates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TeleOp.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleops;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp")
+public class TeleOp extends CommandOpMode {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
