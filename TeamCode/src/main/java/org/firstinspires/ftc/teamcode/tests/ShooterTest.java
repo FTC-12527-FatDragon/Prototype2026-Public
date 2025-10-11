135ca93c359a16e08ab75e919bfe38e06df4d9b8
zihanpeter <luzihan1104@163.com>
2025-10-11 15:38:55 +0800

    color sensor test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ShooterTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "ShooterTest")
+public class ShooterTest extends LinearOpMode {
+    Motor leftShooter;
+    Motor rightShooter;
+
+    @Override
+    public void runOpMode() {
+
+        leftShooter = new Motor(hardwareMap, "leftShooter");
+        rightShooter = new Motor(hardwareMap, "rightShooter");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            rightShooter.set(-1);
+            leftShooter.set(1);
+        }
+
+    }
+}
