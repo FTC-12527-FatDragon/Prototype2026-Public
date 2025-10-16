6e790ad9cdd3804717850174d7e95febc7957921
zihanpeter <luzihan1104@163.com>
2025-10-17 02:02:47 +0800

    motors run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MotorsRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MotorsRun.java
@@ -8,32 +8,24 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "MotorsRun")
 @Config
 public class MotorsRun extends LinearOpMode {
-    public static String motor0Name = "";
-    public static String motor1Name = "";
-    public static String motor2Name = "";
-    public static String motor3Name = "";
+    public static String[] motorName = {"", "", "", ""};
 
-    public static double motor0Power;
-    public static double motor1Power;
-    public static double motor2Power;
-    public static double motor3Power;
+    public static double[] motorPower = new double[4];
 
-    public Motor motor0, motor1, motor2, motor3;
+    Motor[] motors = new Motor[4];
 
     @Override
     public void runOpMode() {
-        if (!motor0Name.isEmpty()) motor0 = new Motor(hardwareMap, motor0Name);
-        if (!motor1Name.isEmpty()) motor1 = new Motor(hardwareMap, motor1Name);
-        if (!motor2Name.isEmpty()) motor2 = new Motor(hardwareMap, motor2Name);
-        if (!motor3Name.isEmpty()) motor3 = new Motor(hardwareMap, motor3Name);
+        for (int i = 0; i < 4; ++i)
+            if (!motorName[i].isEmpty())
+                motors[i] = new Motor(hardwareMap, motorName[i]);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            if (!motor0Name.isEmpty()) motor0.set(motor0Power);
-            if (!motor1Name.isEmpty()) motor1.set(motor1Power);
-            if (!motor2Name.isEmpty()) motor2.set(motor2Power);
-            if (!motor3Name.isEmpty()) motor3.set(motor3Power);
+            for (int i = 0; i < 4; ++i)
+                if (!motorName[i].isEmpty())
+                    motors[i].set(motorPower[i]);
         }
     }
 }
