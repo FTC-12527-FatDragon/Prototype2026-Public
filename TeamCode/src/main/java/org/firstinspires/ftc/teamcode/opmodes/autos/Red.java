8f0ad8c83b2db6bc5a28f5aad872862fc53772f6
YCY5449 <469779590@qq.com>
2025-11-23 03:03:02 +0800

    constraint bug fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/Red.java
@@ -1,40 +1,60 @@
 package org.firstinspires.ftc.teamcode.opmodes.autos;
 
-import static org.firstinspires.ftc.teamcode.tests.Tuning.follower;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.commands.IntakeCommand;
+import org.firstinspires.ftc.teamcode.commands.autoCommands.AutoDriveCommand;
+import org.firstinspires.ftc.teamcode.commands.TransitCommand;
+import org.firstinspires.ftc.teamcode.subsystems.cds.CDS;
 import org.firstinspires.ftc.teamcode.subsystems.drive.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDriveOTOS;
+import org.firstinspires.ftc.teamcode.subsystems.intake.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.shooter.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.transit.Transit;
 
 @Config
 @Autonomous(name = "Red", group = "Autos")
 public class Red extends AutoCommandBase {
-    Follower follower;
+    private MecanumDriveOTOS drive;
+    private Shooter shooter;
+    private Transit transit;
+    private Intake intake;
+    private CDS cds;
+    private Telemetry telemetryM;
+    private Follower follower;
 
 
-    public PathChain Path1;
-    public PathChain Path2;
-    public PathChain Path3;
-    public PathChain Path4;
-    public PathChain Path5;
-    public PathChain Path6;
-    public PathChain Path7;
-    public PathChain Path8;
-    public PathChain Path9;
-    public PathChain Path10;
+    public PathChain path1;
+    public PathChain path2;
+    public PathChain path3;
+    public PathChain path4;
+    public PathChain path5;
+    public PathChain path6;
+    public PathChain path7;
+    public PathChain path8;
+    public PathChain path9;
+    public PathChain path10;
+
+    private Command initializeCommand() {
+        return new SequentialCommandGroup();
+    }
 
     @Override
     public Command runAutoCommand() {
         follower = Constants.createFollower(hardwareMap);
 
-        Path1 = follower
+        path1 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(104.864, 134.467), new Pose(84.627, 83.958))
@@ -42,7 +62,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(225))
                 .build();
 
-        Path2 = follower
+        path2 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(84.627, 83.958), new Pose(102.188, 83.791))
@@ -50,7 +70,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(225), Math.toRadians(0))
                 .build();
 
-        Path3 = follower
+        path3 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(102.188, 83.791), new Pose(120.084, 83.958))
@@ -58,7 +78,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                 .build();
 
-        Path4 = follower
+        path4 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(120.084, 83.958), new Pose(84.794, 83.958))
@@ -66,7 +86,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(225))
                 .build();
 
-        Path5 = follower
+        path5 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(84.794, 83.958), new Pose(103.024, 60.042))
@@ -74,7 +94,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(225), Math.toRadians(0))
                 .build();
 
-        Path6 = follower
+        path6 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(103.024, 60.042), new Pose(119.749, 60.042))
@@ -82,7 +102,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                 .build();
 
-        Path7 = follower
+        path7 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(119.749, 60.042), new Pose(84.627, 83.958))
@@ -90,7 +110,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(225))
                 .build();
 
-        Path8 = follower
+        path8 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(84.627, 83.958), new Pose(103.693, 35.624))
@@ -98,7 +118,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(225), Math.toRadians(0))
                 .build();
 
-        Path9 = follower
+        path9 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(103.693, 35.624), new Pose(120.418, 35.624))
@@ -106,7 +126,7 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                 .build();
 
-        Path10 = follower
+        path10 = follower
                 .pathBuilder()
                 .addPath(
                         new BezierLine(new Pose(120.418, 35.624), new Pose(78.272, 16.557))
@@ -114,6 +134,51 @@ public class Red extends AutoCommandBase {
                 .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(240))
                 .build();
 
-        return new SequentialCommandGroup();
+        return new SequentialCommandGroup(
+                initializeCommand(),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.SLOW)),
+                new AutoDriveCommand(follower, path1),
+                //new AutoShootCommand(transit, intake, shooter),
+                new ParallelCommandGroup(
+                        new TransitCommand(transit, intake, shooter),
+                        new WaitUntilCommand(() -> shooter.getBalls() < 1)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.STOP)),
+                new AutoDriveCommand(follower, path2),
+                new ParallelCommandGroup(
+                        new AutoDriveCommand(follower, path3),
+                        new IntakeCommand(transit, intake)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.SLOW)),
+                new AutoDriveCommand(follower, path4),
+                new ParallelCommandGroup(
+                        new TransitCommand(transit, intake, shooter),
+                        new WaitUntilCommand(() -> shooter.getBalls() < 1)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.STOP)),
+                new AutoDriveCommand(follower, path5),
+                new ParallelCommandGroup(
+                        new AutoDriveCommand(follower, path6),
+                        new IntakeCommand(transit, intake)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.SLOW)),
+                new AutoDriveCommand(follower, path7),
+                new ParallelCommandGroup(
+                        new TransitCommand(transit, intake, shooter),
+                        new WaitUntilCommand(() -> shooter.getBalls() < 1)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.STOP)),
+                new AutoDriveCommand(follower, path8),
+                new ParallelCommandGroup(
+                        new AutoDriveCommand(follower, path9),
+                        new IntakeCommand(transit, intake)
+                ),
+                new InstantCommand(() -> shooter.setShooterState(Shooter.ShooterState.FAST)),
+                new AutoDriveCommand(follower, path10),
+                new ParallelCommandGroup(
+                        new TransitCommand(transit, intake, shooter),
+                        new WaitUntilCommand(() -> shooter.getBalls() < 1)
+                )
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/Constants.java
@@ -78,16 +78,16 @@ public class Constants {
      The BEZIER_CURVE_SEARCH_LIMIT should typically be left at 10 and shouldn't be changed.
      */
 
-    public static PathConstraints pathConstraints = new PathConstraints(
-            0.995,
-            0.1,
-            0.1,
-            0.009,
-            50,
-            1.25,
-            10,
-            1
-    );
+//    public static PathConstraints pathConstraints = new PathConstraints(
+//            0.995,
+//            0.1,
+//            0.1,
+//            0.009,
+//            50,
+//            1.25,
+//            10,
+//            1
+//    );
 
     //Add custom localizers or drivetrains here
     public static Follower createFollower(HardwareMap hardwareMap) {
@@ -95,7 +95,7 @@ public class Constants {
                 .mecanumDrivetrain(driveConstants)
                 .OTOSLocalizer(otosConstants)
 //                .pinpointLocalizer(localizerConstants)
-                .pathConstraints(pathConstraints)
+//                .pathConstraints(pathConstraints)
                 .build();
     }
 }
