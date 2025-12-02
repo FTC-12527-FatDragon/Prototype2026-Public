f88e369550e73e517a1c5cb9fd26be52c2f8f031
Adoramis <luzihan1104@163.com>
2025-12-02 23:58:16 +0800

    Update TransitConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transit/TransitConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transit/TransitConstants.java
@@ -11,8 +11,8 @@ public class TransitConstants {
     public static double limitServoClosePos = 0.29;
     public static double limitServoOpenPos = 0.03;
 
-    public static double chooseServoClosePos = 0.2;
-    public static double chooseServoOpenPos = 0.02;
+    public static double chooseServoClosePos = 0.0;
+    public static double chooseServoOpenPos = 0.0;
 
     public static double transitIntakePower = 0.4;
     public static double transitShootPower = 1.0;
